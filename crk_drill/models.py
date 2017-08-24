8a9810688bfe30f6889a3860091cb787ce065f39
bpancheshen <bpancheshen@gmail.com>
2017-08-23 19:04:02 -0600

    Added class Tag to crk_drill.models.py

--- crk_drill/models.py
+++ crk_drill/models.py
@@ -11,7 +11,6 @@ class User(auth.models.User, auth.models.PermissionsMixin):
 
 class Word(models.Model):
 
-
     wordid = models.AutoField(primary_key=True)
     language = models.CharField(max_length=5, default=LEARNING_LANG, db_index=True)
     wordform = models.CharField(max_length=200, db_index=True)
@@ -38,3 +37,65 @@ class Lemma(models.Model):
 
     def __str__(self):
         return self.lemma
+
+ class Tag(models.Model):
+    '''
+    These tags are used by the fst to compute the forms.
+    They will be different for every language and all must be included.
+    For each field, the possible contents are defined in
+    ../LANGCODE/meta/tags.txt
+    for each here, the possible crk tags will be commented
+    '''
+
+    #the full string of the tag. Should be valid to the fst.
+    string = models.CharField(max_length=40, unique=True)
+ 	pos = models.CharField(max_length=12)
+
+    #only applies to nouns
+
+    #AN or IN
+ 	animacy = models.CharField(max_length=5)
+    #Sg or Pl
+ 	number = models.CharField(max_length=5)
+    #Der/Dim or whether it's a diminutive or not
+ 	derivation = models.CharField(max_length=7)
+
+    #only applies to verbs
+
+    #AI, II, TA, TI
+ 	trans_anim = models.CharField(max_length=5)
+    #Imp, Cnj, Ind, Cond
+ 	mood = models.CharField(max_length=5)
+    #12Pl, 1Pl, 1Sg, etc
+    personnumber = models.CharField(max_length=8)
+    #PV/e
+ 	preverb = models.CharField(max_length=8)
+    #3SgO, 4SgO, 3PlO, etc, the personnumber of the object for TA verbs
+ 	object = models.CharField(max_length=12)
+    #Neg
+ 	polarity = models.CharField(max_length=5)
+    #Ind, Cnj
+ 	mode = models.CharField(max_length=7)
+    #Prs, Prt, Fut
+ 	tense = models.CharField(max_length=5)
+
+ 	intentional_definite = models.CharField(max_length=5)
+ 	attributive = models.CharField(max_length=5)
+ 	case = models.CharField(max_length=5)
+ 	# conneg = models.CharField(max_length=5)
+ 	grade = models.CharField(max_length=10)
+ 	infinite = models.CharField(max_length=10)
+
+ 	# language = models.CharField(max_length=8)
+
+    #applies to other parts of speech
+
+    #Px12Pl, Px1Pl, Px2Pl.. etc
+ 	possessive = models.CharField(max_length=10)
+    #Prox/Med/Dist
+ 	distance = models.CharField(max_length=7)
+    #Prop, Pers, Dem, Interr, Refl, Card
+ 	subclass = models.CharField(max_length=10)
+
+    #This model is from oahpa, these are forms that are not valid for crk
+ 	gender = models.CharField(max_length=5)
